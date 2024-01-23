783eddf2a64937fd6e32035fa5ee340725f8076e
Brayan Riveros <brayan.riveros@jbs-apps.com>
2024-01-23 15:47:47 -0500

    Error send email

--- domain/mailAzure.py
+++ domain/mailAzure.py
@@ -4,17 +4,20 @@ import os
 
 
 def send_mail(recipients: Recipients, subject: str, content: str):
-    connection_string = os.environ.get("connection_communication_services")
-    client = EmailClient.from_connection_string(connection_string)
-    message = {
-        "senderAddress": "DoNotReply@9166101c-80c3-42b4-9977-2d1950c42921.azurecomm.net",
-        "recipients": recipients.dict(),
-        "content": {
-            "subject": subject,
-            "plainText": content,
+    try:
+        connection_string = os.environ.get("connection_communication_services")
+        client = EmailClient.from_connection_string(connection_string)
+        message = {
+            "senderAddress": "DoNotReply@9166101c-80c3-42b4-9977-2d1950c42921.azurecomm.net",
+            "recipients": recipients.dict(),
+            "content": {
+                "subject": subject,
+                "plainText": content,
+            }
         }
-    }
 
-    poller = client.begin_send(message)
-    result = poller.result()
-    return result
+        poller = client.begin_send(message)
+        result = poller.result()
+        return result
+    except Exception:
+        print("It could not send email")
